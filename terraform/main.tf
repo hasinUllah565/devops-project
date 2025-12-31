b8faa8bfc381b0583a7f47e7190da7c19ee6c8c5
Hasin <hassnullah9523@gmail.com>
2025-12-31 22:04:34 +0500

    Initial DevOps project

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "my_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  key_name               = "aws-key"
+
+  tags = {
+    Name = "DevOps-EC2"
+  }
+}
+
