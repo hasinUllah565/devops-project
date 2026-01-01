02182caab7d4a83881e2c9ef58709d438c99098b
Hasin <hassnullah9523@gmail.com>
2026-01-01 11:05:53 +0500

    changes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "DevOps Project Deployed Successfully!"
 
-app.run(host="127.0.0.2", port=5000)
+app.run(host="0.0.0.0", port=5000)
 
